54c46cd0347c127bdd60926e60e09989bb9122b8
AneeshRijhwani25 <7851950798aneesh@gmail.com>
2023-04-12 15:56:08 +0530

    Prepared User model

--- src/models/user.js
+++ src/models/user.js
@@ -19,7 +19,8 @@ module.exports = (sequelize, DataTypes) => {
       allowNull:false,
       unique:true,
       validate:{
-        isEmail:true
+        isEmail:true,
+        isFinite:true
       }
     },
     password: {
